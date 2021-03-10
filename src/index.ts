3dfc4063be1b4738421712a60de6b6e08b94e56b
kenmueller <kenmueller0@gmail.com>
2021-03-10 15:56:08 -0800

    remove old prizes

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,6 @@
 import env from 'dotenv'
 env.config()
 
-import firebase from './firebase'
-import prizes from './prizes'
-import getImage from './image'
+import { removePrizes, updatePrizes } from './prizes'
 
-const firestore = firebase.firestore()
-const storage = firebase.storage().bucket()
-
-Promise.all(
-	Object.entries(prizes).map(async ([id, data]) => {
-		const image = await getImage(id)
-
-		await Promise.all([
-			firestore.doc(`prizes/${id}`).set(data),
-			storage.file(`prizes/${id}`).save(image.data, {
-				public: true,
-				gzip: true,
-				metadata: {
-					contentType: image.type,
-					contentDisposition: `inline; filename=${JSON.stringify(data.name)}`,
-					cacheControl: 'public, max-age=31536000, s-maxage=31536000'
-				}
-			})
-		])
-	})
-)
+Promise.all([removePrizes(), updatePrizes()])
--- src/prizes.ts
+++ src/prizes.ts
@@ -1,4 +1,7 @@
-export interface Prize {
+import firebase from './firebase'
+import getImage from './image'
+
+interface Prize {
 	link: string
 	name: string
 	bits: number
@@ -7,4 +10,37 @@ export interface Prize {
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const prizes: Record<string, Prize> = require('../data/prizes.json')
 
-export default prizes
+const firestore = firebase.firestore()
+const storage = firebase.storage().bucket()
+
+export const removePrizes = async () => {
+	const newPrizes = Object.keys(prizes)
+	const { docs: oldPrizes } = await firestore.collection('prizes').get()
+
+	await Promise.all(
+		oldPrizes
+			.filter(({ id }) => !newPrizes.includes(id))
+			.map(({ ref }) => ref.delete())
+	)
+}
+
+export const updatePrizes = async () => {
+	await Promise.all(
+		Object.entries(prizes).map(async ([id, data]) => {
+			const image = await getImage(id)
+
+			await Promise.all([
+				firestore.doc(`prizes/${id}`).set(data),
+				storage.file(`prizes/${id}`).save(image.data, {
+					public: true,
+					gzip: true,
+					metadata: {
+						contentType: image.type,
+						contentDisposition: `inline; filename=${JSON.stringify(data.name)}`,
+						cacheControl: 'public, max-age=31536000, s-maxage=31536000'
+					}
+				})
+			])
+		})
+	)
+}
